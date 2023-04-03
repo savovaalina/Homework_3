ab3ff93e66c8328cf7ec05a7d8748ac15902fa8d
Your Name <you@example.com>
2023-04-03 15:37:36 +0200

    third

--- Homework_3/Program.cs
+++ Homework_3/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int a = 4;
-int b = 2;
-int c = a + b;
+int a = 5;
+int b = 5;
+int c = a * b;
 
 Console.WriteLine("Rezultatot e: {0}", c);
